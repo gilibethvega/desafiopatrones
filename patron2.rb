4a0b77d984bc8521dc93fb63cc896b915883d7cd
Gilibeth Vega <gilibethvega@gmail.com>
2020-10-24 14:41:03 -0300

    se termina desafio

--- patron2.rb
+++ patron2.rb
@@ -1,10 +1,11 @@
 n = ARGV[0].to_i
 
-(n/2).times do |i|
-  if i%2 == 1
-    print ".."
+n.times do |i|
+  if i%4 == 0 || i%4 == 1
+    print "*"
   else
-    print "**"
+    
+    print "."
   end
 end
 print "\n"
